b5672a95115348216c90838ff17260d01ec372c9
Nick-Archi <nicholas.archibong@gmail.com>
2016-12-02 17:17:33 -0600

    CheckMemory and UserLogin Modules Added

---
+++ CheckMemory.v
@@ -0,0 +1,17 @@
+/* 	Module: CheckMemory
+*
+* 	Description: Takes the user’s ID and checks the memory in order to access the user’s specified level 
+* (if not a guest) and high score. (RAM/ROM)
+*
+*	Created By: Nicholas
+*
+*/
+
+module CheckMemory(password, displayLevel);
+
+
+
+
+
+
+endmodule
---
+++ UserLogin.v
@@ -0,0 +1,7 @@
+/* 	Module: UserLogin
+* 	Description: Takes the user’s inputted password and 
+*	sends this 4 digit value to the CheckMemory module.
+*
+*	Created By: Nicholas
+*
+*/
