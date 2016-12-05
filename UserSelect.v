7177ef98d17c4423c893a73c2de326f4d6ad5531
Nick-Archi <nicholas.archibong@gmail.com>
2016-12-04 23:01:24 -0600

    Added the pause signal
    Added the pause signal to the UserSelect module in order to let it be
    determined by the Pause module.

--- UserSelect.v
+++ UserSelect.v
@@ -1,20 +1,20 @@
 /* 	Module: UserSelect
 *
 * 	Description: Controls the TimerSpeed and UserLogin modules through the selection of a toggle switch 
-*	on the board that is checked during reset. If the user wants to continue as a guest 
-*	then the switch should be pulled down, if not then it is assumed that 
+*	on the board that is checked during reset and after the pause stage. If the user wants to continue as a guest 
+*	then the switch should've been pulled down during the pause stage, if not then it is assumed that 
 *	the user has a known password and will be prompted for this. 
 *
 *	Created By: Nicholas
 *
 */
 
-module UserSelect(toggle, ready, clk, rst);
+module UserSelect(toggle, pause, ready, clk, rst);
 
 	/* inputs
 	*	toggle = switch that determines if the user is a guest or not
 	*/
-	input toggle, clk, rst;
+	input toggle, pause, clk, rst;
 	
 	/* outputs/reg
 	*	ready = notifies the other modules of the decision made by the user
@@ -30,35 +30,51 @@ module UserSelect(toggle, ready, clk, rst);
 	/* parameter
 	*	
 	*/	
-	parameter sWait = 0, s1 = 1, s2 = 2;
+	parameter sWait = 0, s1 = 1, sDone = 2;
 	
 	always@(posedge clk) begin
 		
 		if(rst == 0) begin
 		
-			if(toggle == 1) begin
-				ready <= 1;	
-			end // end of toggle if
-			
-			else begin
-				ready <= 0;
-			end
+			ready <= 0;
+			state <= sWait;
 			
 		end // end of rst if
 		
 		/*
-		*	FSM used to continue outputting the decision by the user... not sure if necessary or not
+		*	
 		*/
 		else begin
 			case(state)
 				
 				sWait: begin
-					ready <= ready;
-					state <= sWait;
+					if(pause == 0) begin // selection has not been made by user yet
+						state <= sWait;
+					end
+					
+					else begin
+						state <= s1;
+					end
+				end // end sWait
+				
+				s1: begin
+					if(toggle == 0) begin // user is a guest
+						ready <= 0;
+					end
+					
+					else begin
+						ready <= 1;
+					end
+					
+					state <= sDone;
+				end
+				
+				sDone: begin
+					//ready <= ready;
 				end
 				
 				default: begin
-					ready <= ready;
+					ready <= 0;
 					state <= sWait;
 				end
 					
