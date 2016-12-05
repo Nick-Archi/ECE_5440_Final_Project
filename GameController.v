ec5482edfc166f43229c4ff3bc24740579d65db5
enduong <eduong0618@gmail.com>
2016-12-05 12:31:41 -0600

    Restart GameController

--- GameController.v
+++ GameController.v
@@ -1,8 +1,8 @@
-module GameController(tog_start, time_out, done, enable, LED, clk, rst);
+module GameController(GO, timeOut, done, seq, LED, clk, rst);
 
-input time_out, enable, clk, rst;
-input [2:0] done;
-input [1:0] tog_start;
+input timeOut, enable, clk, rst;
+input [2:0] seq;
+input GO;
 
 output reg [2:0] LED;
 
@@ -12,26 +12,6 @@ always @(posedge clk) begin
 		LED <= 3'b000;
 	end
 	else begin 
-		case(tog_start)
-			2'b00: begin
-				if(time_out == 0 && enable == 1) begin	//level: Normal
-					LED <= done;
-					end
-			end
-			2'b01: begin
-				if(time_out == 0 && enable == 1) begin //level: Intermediate
-					LED <= done;
-				end
-			end
-			2'b11: begin
-				if(time_out == 0 && enable == 1) begin //level: Advance
-					LED <= done;
-				end
-			end
-			default: begin
-				LED <= 3'b000;
-			end
-		endcase
 	end
 end
 endmodule
